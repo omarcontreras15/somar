cb3065cfcb4501a34411329003de8a3e0f0a651b
Omar Contreras <omararturo15@hotmail.com>
2017-05-31 18:51:36 -0500

    Modificacion de la arquitectura

--- admin.php
+++ admin.php
@@ -1,18 +1,18 @@
 <?php
 
-include_once "./app/router/user/router.php";
+include_once "./app/router/admin/router.php";
 //inicia la sesion 
 session_start();
 //verifica que no se haya excedido el tiempo limite inactividad para la sesion
 //si se excedio se destruye la sesion y se redirige al login
-if(isset($_SESSION["user_id"])){
+if(isset($_SESSION["admin_id"])){
     $timeActual=time();
     $diferencia=$timeActual-$_SESSION["ultimoAcceso"];
     $_SESSION["ultimoAcceso"]=$timeActual;
     //si la inactividad es mayor a 30 minutos se destruye la sesion
     if($diferencia>=1800){
         session_destroy();
-        header("Location: index.php?mode=iniciarSesion");
+        header("Location: admin.php?mode=iniciarSesion");
     }
 }
 $router = new Router();
